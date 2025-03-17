d7ef0af2481f8aafa1d394058cdba0be215e0904
Tonmoy Talukder <tonmoytalukder2000@gmail.com>
2025-03-17 21:36:02 +0600

    Cors, global prefix, rate limits

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -10,7 +10,9 @@ import {
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma, Role } from '@prisma/client';
+import { Throttle, SkipThrottle } from '@nestjs/throttler';
 
+@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
@@ -20,11 +22,14 @@ export class EmployeesController {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @SkipThrottle({ default: false })
   @Get()
-  findAll(@Query('role') role?: Role) {   // 'INTERN' | 'ENGINEER' | 'ADMIN'
+  findAll(@Query('role') role?: Role) {
+    // 'INTERN' | 'ENGINEER' | 'ADMIN'
     return this.employeesService.findAll(role);
   }
 
+  @Throttle({ short: { ttl: 1000, limit: 1 } })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,25 @@ import { AppModule } from './app.module';
 //   cert: fs.readFileSync('./secrets/cert.crt'),
 // };
 
+const allowedOrigins = ['https://example.com', 'https://another.com'];
+
+const corsOption = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); //{httpsOptions,}
+  app.enableCors(); // open for all, if corsOption is inside that it will be limited
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
